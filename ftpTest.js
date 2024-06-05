057f039814bbf3c43e96b326a05e17a0c0d1faa0
caamaño lucas <caamano.l@opendevpro.com>
2024-06-05 13:40:55 -0300

    Create ftpTest.js

---
+++ ftpTest.js
@@ -0,0 +1,14 @@
+let sftp = new Client();
+ 
+sftp.connect({
+  host: '127.0.0.1',
+  port: '8080',
+  username: 'username',
+  password: 'saraza'
+}).then(() => {
+  return sftp.list('/pathname');
+}).then(data => {
+  console.log(data, 'the data info');
+}).catch(err => {
+  console.log(err, 'catch error');
+});``
